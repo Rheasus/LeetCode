670b1fe2ffcf7c363a8e8d1e5a6073be321d1636
Rheasus <33912265+Rheasus@users.noreply.github.com>
2022-10-11 13:34:20 +0300

    Create firstBadVersion.py

---
+++ firstBadVersion.py
@@ -0,0 +1,13 @@
+def firstBadVersion(n):
+    m = n // 2
+    l = 0
+    r = n
+
+    while l < m:
+        if isBadVersion(m) == 0:
+            l = m
+        else:
+            r = m
+        m = (l + r) // 2
+
+    return (m + 1)
